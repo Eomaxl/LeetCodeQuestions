0dacb512f5d345993d94ad21bb3e6227569bff21
Eomaxl <souravsnigdh@gmail.com>
2024-01-03 20:14:48 +0530

    [String] MaxNestingParentheses

--- src/com/company/string/MaxNestingDepthParentheses.java
+++ src/com/company/string/MaxNestingDepthParentheses.java
@@ -1,4 +1,18 @@
 package com.company.string;
 
 public class MaxNestingDepthParentheses {
+    public int maxDepth(String s) {
+        char[] chars = s.toCharArray();
+        int count = 0, maxCount = 0;
+        for (char c: chars){
+            if (c == '('){
+                count++;
+                maxCount = Math.max(maxCount, count);
+            }
+            if (c == ')') {
+                count--;
+            }
+        }
+        return maxCount;
+    }
 }
