fda62de7d864d62f3e781e32941b6c5de624f4d3
Eomaxl <souravsnigdh@gmail.com>
2024-01-03 20:13:47 +0530

    [String] SortCharByFrequency

--- src/com/company/string/SortCharByFrequency.java
+++ src/com/company/string/SortCharByFrequency.java
@@ -1,4 +1,27 @@
 package com.company.string;
-
+import java.util.*;
 public class SortCharByFrequency {
+    public String frequencySort(String s) {
+        if( s == null || s.isEmpty()) return s;
+        char[] chars = s.toCharArray();
+        Arrays.sort(chars);
+
+        List<String> charString = new ArrayList<String>();
+        StringBuilder sb = new StringBuilder();
+        sb.append(chars[0]);
+        for(int i =1; i< chars.length; i++){
+            if(chars[i] != chars[i-1]){
+                charString.add(sb.toString());
+                sb = new StringBuilder();
+            }
+            sb.append(chars[i]);
+        }
+        charString.add(sb.toString());
+        Collections.sort(charString, (a,b) -> b.length() - a.length());
+        StringBuilder obj = new StringBuilder();
+        for(String str: charString)
+            obj.append(str);
+
+        return obj.toString();
+    }
 }
